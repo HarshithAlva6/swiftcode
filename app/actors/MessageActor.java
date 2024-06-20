e2bbdc32ddde67d14899c2a11ba956a4de3d4467
Harshith <harshalva.titan@csu.fullerton.edu>
2024-06-19 21:08:46 -0700

    swiftcode at Betsol

---
+++ app/actors/MessageActor.java
@@ -0,0 +1,51 @@
+package actors;
+
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import data.FeedResponse;
+import data.Message;
+import services.FeedService;
+import services.NewsAgentService;
+
+import java.util.UUID;
+
+public class MessageActor extends UntypedActor {
+    private final ActorRef out;
+
+    public static Props props(ActorRef out) {
+        return Props.create(MessageActor.class, out);
+    }
+
+    public MessageActor(ActorRef out) {
+        this.out = out;
+    }
+
+    private FeedService feedService = new FeedService();
+    private NewsAgentService newsAgentService = new NewsAgentService();
+
+    @Override
+    public void onReceive(Object message) throws Throwable {
+        ObjectMapper mapper = new ObjectMapper();
+        Message messageObj = new Message();
+        if (message instanceof String) {
+            messageObj.text=(String)message;
+            messageObj.sender=Message.Sender.USER;
+            out.tell(mapper.writeValueAsString(messageObj), self());
+            String query = newsAgentService.getNewsAgentResponse("Find " +message,UUID.randomUUID()).query;
+            FeedResponse feedResponse = feedService.getFeedByQuery(query);
+            messageObj.text = (feedResponse.title == null) ? "No results found" : "Showing results for: " + query;
+            messageObj.feedResponse=feedResponse;
+            messageObj.sender=Message.Sender.BOT;
+            out.tell(mapper.writeValueAsString(messageObj), self());
+        }
+        else
+        {
+            messageObj.text = "Text is invalid input";
+            messageObj.sender=Message.Sender.BOT;
+            out.tell(mapper.writeValueAsString(messageObj), self());
+        }
+
+    }
+}
---
+++ public/javascripts/chat.js
@@ -0,0 +1,27 @@
+var app = angular.module('chatApp', ['ngMaterial']);
+
+app.config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('cyan')
+        .accentPalette('cyan');
+});
+
+app.controller('chatController', function ($scope, $sce) {
+    $scope.messages = [];
+    $scope.trust = $sce.trustAsHtml;
+
+    var exampleSocket = new WebSocket("wss://swiftcode-harshith.herokuapp.com/chatSocket");
+    exampleSocket.onmessage = function (event) {
+        var jsonData = JSON.parse(event.data);
+        jsonData.time = new Date()
+            .toLocaleTimeString();
+        $scope.messages.push(jsonData);
+        $scope.$apply();
+        console.log(jsonData);
+    };
+
+    $scope.sendMessage = function () {
+        exampleSocket.send($scope.userMessage);
+        $scope.userMessage = "";
+    };
+});
